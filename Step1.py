9cb31d99eb17f69a1326e698ca6c957a15653c8b
Haodong Chen <hdchenmsn@hotmail.com>
2014-10-16 09:56:00 -0700

    Version 1.0

--- Step1.py
+++ Step1.py
@@ -20,25 +20,9 @@
 
 import os, sys, subprocess
 from optparse import OptionParser
+from bs_tools_utils.utils import *
 global Params
 Params={}
-
-def Conf_read(conf_path):
-    try:
-        conf = open(conf_path,'r')
-    except:
-        print >> sys.stderr, 'ERROR: Cannot open configuration file conf.txt'
-        exit(1)
-    for line in conf:
-        if line[0] != "#":
-            line=line.rstrip().replace(" ", "")
-            if len(line) == 0: continue
-            # Format for conf.txt:
-            # variable=value
-            variable=line.split("=")[0]
-            value=line.split("=")[1]
-            Params[variable]=value
-    return True
 
 def Step1():
     # Demultiplex
@@ -50,18 +34,19 @@ def Step1():
     Conf_read(options.CONFPATH)
     if Params['MULTIPLEX'] != "False":
         Params['BARCODES'] = ','.join([str(int(x)) for x in Params['BARCODES'].split(",")])
-        file_list_1 = [x for x in options.folder if \
+        file_list_1 = [x for x in os.listdir(options.folder) if \
                        (x.endswith("_qseq.txt") or x.endswith("_qseq.txt.gz")) \
                        and x.split("_")[-3] == "1"]
         file_list_1.sort()
-        f1 = file_list_1[int(os.environ["$SGE_TASK_ID"]) - 1]
+        f1 = file_list_1[int(os.environ["SGE_TASK_ID"]) - 1]
         f1name = f1.split("_")
         f1name[-3] = "2"
         f2 = "_".join(f1name)
-        p = '-I %s -s %s -b %s -l %s -m %s'%(os.getcwd(), f1, f2, Params['BARCODES'], Params['BCMISMATCH'])
-        cmd = [os.path.join(Params['BSPATH'],"MyDemultiplex.py")]
+        p = '-I %s -s %s -b %s -l %s -m %s'%(options.folder, f1, f2, Params['BARCODES'], Params['BCMISMATCH'])
+        cmd = ["MyDemultiplex.py"]
         cmd.extend(p.split(" "))
-        process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        #print >> sys.stdout, " ".join(cmd)
+        process = subprocess.Popen(" ".join(cmd), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         out, err = process.communicate()
         print >> sys.stdout, out
 
--- Step2.py
+++ Step2.py
@@ -20,25 +20,9 @@
 
 import os, sys, subprocess
 from optparse import OptionParser
+from bs_tools_utils.utils import *
 global Params
 Params={}
-
-def Conf_read(conf_path):
-    try:
-        conf = open(conf_path,'r')
-    except:
-        print >> sys.stderr, 'ERROR: Cannot open configuration file conf.txt'
-        exit(1)
-    for line in conf:
-        if line[0] != "#":
-            line=line.rstrip().replace(" ", "")
-            if len(line) == 0: continue
-            # Format for conf.txt:
-            # variable=value
-            variable=line.split("=")[0]
-            value=line.split("=")[1]
-            Params[variable]=value
-    return True
 
 def Step2():
     """Align using bs-seeker2"""
@@ -51,14 +35,14 @@ def Step2():
     file_list = [x for x in os.listdir(options.folder) \
                  if x.endswith("qseq.txt") or x.endswith("qseq.txt.gz")]
     file_list.sort()
-    f = file_list[int(os.environ["$SGE_TASK_ID"]) - 1]
+    f = file_list[int(os.environ["SGE_TASK_ID"]) - 1]
     cmd = os.path.join(Params['BSPATH'],"bs_seeker2-align.py")
     p = [cmd]
     p.extend( ("-i %s"%os.path.join(options.folder, f)).split(" "))
     p.extend( ("-L %s -U %s -m %s"%(Params['LOW_BOUND'], Params['UP_BOUND'], Params['NO_MISMATCHES'])).split(" ") )
     p.extend( ("-g %s -d %s -p %s"%(Params['GENOME'], Params['DBPATH'], Params['ALIGNER_PATH'])).split(" "))
-    p.extend( ["-r", "-a", "adapter.txt", "--aligner", "bowtie2"])
-    process = subprocess.Popen(p, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    p.extend( ["-r", "-a", os.path.join(Params['BSPATH'], "adapter.txt"), "--aligner", "bowtie2", "--bt2--end-to-end"])
+    process = subprocess.Popen(" ".join(p), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, err = process.communicate()
     print >> sys.stdout, out
 
