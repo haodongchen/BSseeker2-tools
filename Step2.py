bbfe6df73353cf75618f62858c599bd87f2e5c32
Haodong Chen <chen.hdong@gmail.com>
2014-10-17 15:50:11 -0700

    Update Step2.py

--- Step2.py
+++ Step2.py
@@ -18,7 +18,7 @@
 # Comments and/or additions are welcome (send e-mail to:
 # hdchen@ucla.edu).
 
-import os, sys, subprocess
+import os, sys, subprocess, math
 from optparse import OptionParser
 from utils import *
 
@@ -33,16 +33,19 @@ def Step2():
     file_list = [x for x in os.listdir(options.folder) \
                  if x.endswith("qseq.txt") or x.endswith("qseq.txt.gz")]
     file_list.sort()
-    f = file_list[int(os.environ["SGE_TASK_ID"]) - 1]
-    cmd = os.path.join(Params['BSPATH'],"bs_seeker2-align.py")
-    p = [cmd]
-    p.extend( ("-i %s"%os.path.join(options.folder, f)).split(" "))
-    p.extend( ("-L %s -U %s -m %s"%(Params['LOW_BOUND'], Params['UP_BOUND'], Params['NO_MISMATCHES'])).split(" ") )
-    p.extend( ("-g %s -d %s -p %s"%(Params['GENOME'], Params['DBPATH'], Params['ALIGNER_PATH'])).split(" "))
-    p.extend( ["-r", "-a", os.path.join(Params['BSPATH'], "adapter.txt"), "--aligner", "bowtie2", "--bt2--end-to-end"])
-    process = subprocess.Popen(" ".join(p), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    out, err = process.communicate()
-    print >> sys.stdout, out
+    FILE_BLOCK = int(math.ceil(float(len(file_list)) / float(os.environ["SGE_TASK_LAST"])))
+    FILE_START = FILE_BLOCK * (int(os.environ["SGE_TASK_ID"])-1)
+    FILE_END = min(len(file_list), FILE_START + FILE_BLOCK)
+    for f in file_list[FILE_START, FILE_END]:
+        cmd = os.path.join(Params['BSPATH'],"bs_seeker2-align.py")
+        p = [cmd]
+        p.extend( ("-i %s"%os.path.join(options.folder, f)).split(" "))
+        p.extend( ("-L %s -U %s -m %s"%(Params['LOW_BOUND'], Params['UP_BOUND'], Params['NO_MISMATCHES'])).split(" ") )
+        p.extend( ("-g %s -d %s -p %s"%(Params['GENOME'], Params['DBPATH'], Params['ALIGNER_PATH'])).split(" "))
+        p.extend( ["-r", "-a", os.path.join(Params['BSPATH'], "adapter.txt"), "--aligner", "bowtie2", "--bt2--end-to-end"])
+        process = subprocess.Popen(" ".join(p), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = process.communicate()
+        print >> sys.stdout, out
 
 if __name__ == "__main__":
     Step2()
